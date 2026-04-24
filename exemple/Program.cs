7823f0e4ae360f82138fde9343a8305570534f5c
michel339571 <tchiniangamichel@gmail.com>
2026-04-24 13:19:08 +0100

    Ajout nouveau taf

---
+++ exemple/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace exemple;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int niveauEssence=5;
+        while (niveauEssence>0)
+        {
+            Console.WriteLine("La voiture roule........ Essence restante :" + niveauEssence);
+            niveauEssence--;
+        }
+        Console.WriteLine("Panne sèche");
+    }
+}
